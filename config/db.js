269e3f9522583e657d017b679451d7f078ee2d93
bgsub <bryanngatshou@gmail.com>
2024-10-08 01:43:48 -0400

    last one frl changed docker compose file

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
+  username: process.env.POSTGRES_USER || 'dvna',
+  password: process.env.POSTGRES_PASSWORD || 'dvna',
+  database: process.env.POSTGRES_DATABASE || 'passw0rd',
   host: process.env.POSTGRES_HOST ||'postgres-db',
   port: process.env.POSTGRES_PORT || 5432,
   dialect: 'postgres'
