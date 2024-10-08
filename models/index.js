3028edefbc4fb3c81cd4b65333ac87dc9fea0aa7
bgsub <bryanngatshou@gmail.com>
2024-10-08 01:39:09 -0400

    last one frl changed docker compose file

--- models/index.js
+++ models/index.js
@@ -21,6 +21,7 @@ sequelize
     console.log('Connection has been established successfully.');
   })
   .catch(function (err) {
+    console.log(config)
     console.log('Unable to connect to the database:', err);
   })
 
