3af2e7efc8d419e4399e7d7ff82bca5a85edb158
bgsub <bryanngatshou@gmail.com>
2024-10-08 02:09:40 -0400

    changed docker compose file

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
-  username: process.env.POSTGRES_USER || dvna,
-  password: process.env.POSTGRES_PASSWORD || dvna,
-  database: process.env.POSTGRES_DATABASE || passw0rd,
+  username: process.env.POSTGRES_USER || 'dvna',
+  password: process.env.POSTGRES_PASSWORD || 'passw0rd',
+  database: process.env.POSTGRES_DATABASE || 'dvna',
   host: process.env.POSTGRES_HOST ||'postgres-db',
   port: process.env.POSTGRES_PORT || 5432,
   dialect: 'postgres'
